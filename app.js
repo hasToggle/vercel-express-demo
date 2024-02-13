833301068bb14855fb7268d7e149843040a53508
Eric <eric@spiritsdontfly.com>
2024-02-13 13:07:55 +0100

    Remove helmet

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require("express");
-const helmet = require("helmet");
 const routes = require("./routes");
 
 const app = express();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-app.use(helmet());
 app.use("/", routes);
 
 // default catch all handler
