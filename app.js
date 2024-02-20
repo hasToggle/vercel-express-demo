9ad03d8381d609b73f0939bdd3407dc48aac3104
Ernst Stolz <eric@spiritsdontfly.com>
2024-02-20 17:41:21 +0100

    add postgres and notes methods

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const express = require("express");
 const routes = require("./routes");
 
 const app = express();
+app.use(express.json());
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use("/", routes);
 
 // default catch all handler
